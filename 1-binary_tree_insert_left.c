a7a721d881a317b9ab0032b3c256c8dc1b302166
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-15 15:41:11 +0200

    add root condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
     if (parent->left != NULL)
     {
+        root->n = value;
         root->left = parent->left;
     }
 
