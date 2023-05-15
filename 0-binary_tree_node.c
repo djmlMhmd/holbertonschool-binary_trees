706fad21e91c2ba6957cbf5003b8d3c37f4041ca
pizzayolow <ralliard.valentin@gmail.com_>
2023-05-15 03:14:34 -0700

    betty friendly

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,19 +1,26 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - This function initialize the binary tree
+ * @parent: the root
+ * @value: the data
+ * Return: the node we need to fill
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *root = NULL;
+	binary_tree_t *root = NULL;
 
-    root = malloc(sizeof(binary_tree_t));
-    if (root == NULL)
-    {
-        return (NULL);
-    }
+	root = malloc(sizeof(binary_tree_t));
+	if (root == NULL)
+	{
+		return (NULL);
+	}
 
-    root->n = value;
-    root->parent = parent;
-    root->left = NULL;
-    root->right = NULL;
+	root->n = value;
+	root->parent = parent;
+	root->left = NULL;
+	root->right = NULL;
 
-    return (root);
+	return (root);
 }
