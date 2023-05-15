17ece912e60a2937a392ef630db888339d9dc4e2
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-15 15:35:56 +0200

    add new condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *root = NULL;
 
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+
     root = malloc(sizeof(binary_tree_t));
     if (root == NULL)
     {
