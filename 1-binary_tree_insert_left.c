d40e6928d411eac3b7ad3a13e622db8786372c44
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-15 15:21:37 +0200

    task 1 Co-authored-by: Pizzayolow <Pizzayolow@users.noreply.github.com>

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - This function initialize the binary tree
+ * @parent: the root
+ * @value: the data
+ * Return: the node we need to fill
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *root = NULL;
+
+    root = malloc(sizeof(binary_tree_t));
+    if (root == NULL)
+    {
+        return (NULL);
+    }
+
+    if (parent->left != NULL)
+    {
+        root->left = parent->left;
+    }
+
+    root->n = value;
+    root->parent = parent;
+    root->left = parent->left;
+    parent->left = root;
+    root->right = NULL;
+
+    return (root);
+}
