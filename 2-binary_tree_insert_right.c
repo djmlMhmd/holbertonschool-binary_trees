97502cb020fe1e6d1119d22940615b28781bdf1b
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-15 16:10:34 +0200

    add del function

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,30 +9,30 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *root = NULL;
+    binary_tree_t *root = NULL;
 
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
 
-	root = malloc(sizeof(binary_tree_t));
-	if (root == NULL)
-	{
-		return (NULL);
-	}
+    root = malloc(sizeof(binary_tree_t));
+    if (root == NULL)
+    {
+        return (NULL);
+    }
 
-	if (parent->right != NULL)
-	{
-		root->right = parent->right;
-		parent->right->parent = root;
-	}
+    if (parent->right != NULL)
+    {
+        root->right = parent->right;
+        parent->right->parent = root;
+    }
 
-	root->n = value;
-	root->parent = parent;
-	root->right = parent->right;
-	parent->right = root;
-	root->left = NULL;
+    root->n = value;
+    root->parent = parent;
+    root->right = parent->right;
+    parent->right = root;
+    root->left = NULL;
 
-	return (root);
+    return (root);
 }
