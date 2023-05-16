1e19077aa3cf439da1041c15ac33445bd68f8fc1
Pizzayolow <ralliard.valentin@gmail.com>
2023-05-16 13:47:12 +0200

    try to handle the probkem

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -39,11 +39,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+    if (tree->left == NULL || tree->right == NULL)
+        return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
-
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
 
 	if (left == right)
 		return (1);
