f24eed38fec7ae6a2c0920b4f2dd200c38ea7532
pizzayolow <ralliard.valentin@gmail.com_>
2023-05-16 04:27:31 -0700

    task 16 betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,29 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - This function
-* @tree: the node
-* Return: the height of the tree
-*/
+ * binary_tree_height - This function
+ * @tree: the node
+ * Return: the height of the tree
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t countleft = 0;
-    size_t countright = 0;
+	size_t countleft = 0;
+	size_t countright = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left == NULL && tree->right == NULL)
-            return (0);
-    
-    countleft = binary_tree_height(tree->left);
-    countright = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-    if (countleft >= countright)
-        return (countleft + 1);
+	countleft = binary_tree_height(tree->left);
+	countright = binary_tree_height(tree->right);
 
-    else
-        return (countright + 1);
+	if (countleft >= countright)
+		return (countleft + 1);
+
+	else
+		return (countright + 1);
 }
 
 /**
@@ -33,17 +33,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left = 0;
-    int right = 0;
+	int left = 0;
+	int right = 0;
 
-	    if (tree == NULL)
-		    return (0);
+	if (tree == NULL)
+		return (0);
 
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
-    
-        if (left == right)
-            return (1);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    return (0);
+	if (left == right)
+		return (1);
+
+	return (0);
 }
