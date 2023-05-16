217be7ea5527fd770e3418d4ad679113638f5d97
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-16 10:14:52 +0200

    add description

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - This function
+ * @tree: the node
+ * Return: the height of the tree
+ */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     if (tree == NULL)
