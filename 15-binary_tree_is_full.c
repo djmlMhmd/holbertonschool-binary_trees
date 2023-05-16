a378dc9f577e37e3019e01ead6365cb842da454e
Pizzayolow <ralliard.valentin@gmail.com>
2023-05-16 13:25:08 +0200

    task 16 is perfect

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,12 +16,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == 0 && tree->right == 0)
 		return (1);
 	if (tree->left != 0 || tree->right != 0)
-
-		binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
-
-	left_full = binary_tree_is_full(tree->left);
-	right_full = binary_tree_is_full(tree->right);
-	return (right_full && left_full);
+	{	
+		left_full = binary_tree_is_full(tree->left);
+		right_full = binary_tree_is_full(tree->right);
+		return (right_full && left_full);
+	}
 	return (0);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,3 +25,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
     else
         return (countright + 1);
 }
+
+/**
+ * binary_tree_is_perfect - This function
+ * @tree: the node
+ * Return: the height of the tree
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    int left = 0;
+    int right = 0;
+
+	    if (tree == NULL)
+		    return (0);
+
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
+    
+        if (left == right)
+            return (1);
+
+    return (0);
+}
