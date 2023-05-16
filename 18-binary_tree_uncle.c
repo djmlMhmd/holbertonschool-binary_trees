da7e24cf46cb0394d8e48570448ce4895adabe5b
pizzayolow <ralliard.valentin@gmail.com_>
2023-05-16 06:27:53 -0700

    task 18 betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - This function
+ * binary_tree_uncle - This function
  * @node: the node
  * Return: the height of the tree
  */
@@ -9,16 +9,16 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent = NULL;
-    binary_tree_t *grandpere = NULL;
+	binary_tree_t *grandpere = NULL;
 
 	if (node == NULL)
 		return (NULL);
 
-    if (node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
 	parent = node->parent;
-    grandpere = parent->parent;
+	grandpere = parent->parent;
 
 	if (grandpere->left == parent)
 	{
