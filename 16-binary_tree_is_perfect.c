2efd61fdbf3861e87453dccaba7b07efc4e702bc
Pizzayolow <ralliard.valentin@gmail.com>
2023-05-16 13:53:33 +0200

    try to handle the task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,30 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - This function
+ * binary_tree_size - This function
  * @tree: the node
  * Return: the height of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t countleft = 0;
-	size_t countright = 0;
-
 	if (tree == NULL)
-		return (0);
+	return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	binary_tree_size(tree->left);
+	binary_tree_size(tree->right);
 
-	countleft = binary_tree_height(tree->left);
-	countright = binary_tree_height(tree->right);
-
-	if (countleft >= countright)
-		return (countleft + 1);
-
-	else
-		return (countright + 1);
+	return (binary_tree_size(tree->left) + (binary_tree_size(tree->right)) + 1);
 }
+
 
 /**
  * binary_tree_is_perfect - This function
@@ -42,8 +34,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
 
 	if (left == right)
 		return (1);
