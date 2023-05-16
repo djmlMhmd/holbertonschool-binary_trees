5c390d26533331741c093b39d00b1f80931b73c4
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-16 10:17:56 +0200

    betty indentation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,11 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    binary_tree_size(tree->left);
-    binary_tree_size(tree->right);
+	binary_tree_size(tree->left);
+	binary_tree_size(tree->right);
 
-    return (binary_tree_size(tree->left) + (binary_tree_size(tree->right)) + 1);
+	return (binary_tree_size(tree->left) + (binary_tree_size(tree->right)) + 1);
 }
