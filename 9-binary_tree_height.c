ed2a9e62f28a363060eba84069189a9f81a2a19c
Pizzayolow <ralliard.valentin@gmail.com>
2023-05-16 09:52:48 +0200

    task 9, height of a tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - This function
+* @tree: the node
+* Return: the height of the tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t countleft = 0;
+    size_t countright = 0;
+
+    if (tree == NULL)
+        return (0);
+
+        if (tree->left == NULL && tree->right == NULL)
+            return (0);
+    
+    countleft = binary_tree_height(tree->left);
+    countright = binary_tree_height(tree->right);
+
+    if (countleft >= countright)
+        return (countleft + 1);
+
+    else
+        return (countright + 1);
+}
