ade16fe99f8cb596208d9db5794f0c8599de7dd0
pizzayolow <ralliard.valentin@gmail.com_>
2023-05-16 01:24:15 -0700

    task 10 betty friendly

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,19 +7,19 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
 	if (tree == NULL)
 		return (0);
 
-    if (tree->parent != NULL)
-    {
-        depth = binary_tree_depth(tree->parent);
-        return (depth + 1);
-    }
+	if (tree->parent != NULL)
+	{
+		depth = binary_tree_depth(tree->parent);
+		return (depth + 1);
+	}
 
-    if (tree->parent == NULL)
-        return (0);
+	if (tree->parent == NULL)
+		return (0);
 
-return (0);
+	return (0);
 }
